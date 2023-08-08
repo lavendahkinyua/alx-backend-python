4e167e5577b97cef7b5b2f8009ec30bda3abb849
lavendahkinyua <lavendah17@gmail.com>
2023-08-08 19:30:28 +0300

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async comprehension module."""
+
+
+import asyncio
+import random
+generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    result = [i async for i in generator()]
+    return result
