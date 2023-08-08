5ad19587c206c356504dd54a733a56f867ed3894
lavendahkinyua <lavendah17@gmail.com>
2023-08-08 13:28:59 +0300

    commit task 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Type-annotated func to_str takes float n as arg,returns the string rep of n."""
+"""Type-annotated func to_str takes float n as arg,return string rep of n."""
 
 
 def to_str(n: float) -> str:
