f96abf3af0bf9b720322bf0215c8a002a00de028
lavendahkinyua <lavendah17@gmail.com>
2023-08-08 13:00:19 +0300

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 """Type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float."""
-function add (a: float, b: float) -> float:
+def add(a: float, b: float) -> float:
     """Return sum of a and b."""
     return a + b
