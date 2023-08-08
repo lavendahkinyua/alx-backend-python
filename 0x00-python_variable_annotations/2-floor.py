88e53c70a16ca5265ab65a68af96fd0b3247585f
lavendahkinyua <lavendah17@gmail.com>
2023-08-08 13:34:39 +0300

    task2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Type-annotated func floor takes float n as arg,return the floor of float n."""
-
+"""Type-annotated func floor takes float n as arg,return the floor of n"""
 
 def floor(n: float) -> int:
     """Return floor of n."""
