dec83fc5a1806da80b7da598e5447708d04b24c4
lavendahkinyua <lavendah17@gmail.com>
2023-08-08 13:10:45 +0300

    solution to task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function floor takes a float n as argument,returns the floor of the float."""
+
+
+def floor(n: float) -> int:
+    """Return floor of n."""
+    return int(n)
