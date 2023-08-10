3ca0db1f2d7a2ef0dad80ac947860ca0b92d865c
lavendahkinyua <lavendah17@gmail.com>
2023-08-10 11:58:43 +0300

    task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,6 @@ generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """Return list of random floats."""
     result = [i async for i in generator()]
     return result
