798140e53820faad4517c6b87bd999d1f5d10550
lavendahkinyua <lavendah17@gmail.com>
2023-08-10 11:56:42 +0300

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,6 @@ from typing import List
 generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() ->  List[float]:
+async def async_comprehension() -> List[float]:
     result = [i async for i in generator()]
     return result
